61c3601ba2f7e25f8ef70565269b3ff1fe2b2e95
Vinicios Marques <venicio.tere@gmail.com>
2026-09-09 20:59:27 -0300

    first commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "@playwright/test";
+
+test('the user login with success', async ( {page}  ) =>{
+    await page.goto('https://www.saucedemo.com/');
+    await expect(page).toHaveTitle('Swag Labs');
+
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page .url()).toBe('https://www.saucedemo.com/inventory.html');
+
+
+    const productTitle = await page.locator('.header_secondary_container > span');
+    await expect(productTitle).toHaveText('Products');
+    
+});
