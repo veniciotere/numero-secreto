8a87cfb576236eda8256a54c63132d25988df5e8
Vinicios Marques <venicio.tere@gmail.com>
2026-09-09 21:36:30 -0300

    Revert "First Commit"
    This reverts commit d63106ca62c2e14abdc47cfc1b139cc426bbb5fa.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,9 +4,12 @@ test('the user login with success', async ( {page}  ) =>{
     await page.goto('https://www.saucedemo.com/');
     await expect(page).toHaveTitle('Swag Labs');
 
+<<<<<<< HEAD
 TESTE
 
 
+=======
+>>>>>>> parent of d63106c (First Commit)
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
 
